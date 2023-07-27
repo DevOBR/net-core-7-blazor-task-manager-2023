b2ae2c7b4a5facc510a414c79402363df11eb809
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2023-07-26 22:04:37 -0600

    Adding MyTasksController with data in memory

---
+++ TaskManager.Backend/Controllers/MyTasksController.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using TaskManager.Share.Entities;
+
+namespace TaskManager.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyTasksController : ControllerBase
+    {
+        private readonly List<MyTask> _myTasks;
+
+        public MyTasksController()
+        {
+            _myTasks = new List<MyTask>
+            {
+                new MyTask { Id = 1, Description = "Learn c#", Date = DateTime.Now },
+                new MyTask { Id = 2, Description = "Learn API .Net Core", Date = DateTime.Now },
+                new MyTask { Id = 3, Description = "Learn Blazor", Date = DateTime.Now },
+                new MyTask { Id = 4, Description = "Learn Databases with Entity Framework", Date = DateTime.Now }
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_myTasks);
+        }
+
+        [HttpPost]
+        public IActionResult Post(MyTask myTask)
+        {
+            _myTasks.Add(myTask);
+            return Ok(myTask);
+        }
+
+        [HttpPut]
+        public IActionResult Put(MyTask myTask)
+        {
+            var task = _myTasks.FirstOrDefault(x => x.Id == myTask.Id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            task.Description = myTask.Description;
+            task.Date = myTask.Date;
+            task.IsCompleted = myTask.IsCompleted;
+
+            return Ok(task);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var task = _myTasks.FirstOrDefault(x => x.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _myTasks.Remove(task);
+            return NoContent();
+        }
+
+    }
+}
---
+++ TaskManager.Share/Entities/MyTask.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Share.Entities
+{
+    public class MyTask
+    {
+        // auto assignable property - primary key table
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; } = null!;
+
+        public DateTime Date { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
+
