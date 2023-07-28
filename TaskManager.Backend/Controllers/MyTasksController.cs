b87567e5f10b6375593c3026d365eccfb0fc8f0b
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2023-07-27 21:18:22 -0600

    Controller using database

--- TaskManager.Backend/Controllers/MyTasksController.cs
+++ TaskManager.Backend/Controllers/MyTasksController.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Build.Framework;
+using TaskManager.Data;
 using TaskManager.Share.Entities;
 
 namespace TaskManager.Backend.Controllers
@@ -13,36 +8,45 @@ namespace TaskManager.Backend.Controllers
     [ApiController]
     public class MyTasksController : ControllerBase
     {
-        private readonly List<MyTask> _myTasks;
+        private readonly DataContext _context;
 
-        public MyTasksController()
+        public MyTasksController(DataContext context)
         {
-            _myTasks = new List<MyTask>
-            {
-                new MyTask { Id = 1, Description = "Learn c#", Date = DateTime.Now },
-                new MyTask { Id = 2, Description = "Learn API .Net Core", Date = DateTime.Now },
-                new MyTask { Id = 3, Description = "Learn Blazor", Date = DateTime.Now },
-                new MyTask { Id = 4, Description = "Learn Databases with Entity Framework", Date = DateTime.Now }
-            };
+            this._context = context;
         }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(_myTasks);
+
+            return Ok(_context.MyTasks.ToList());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var task = _context.MyTasks.FirstOrDefault(x => x.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
         }
 
         [HttpPost]
         public IActionResult Post(MyTask myTask)
         {
-            _myTasks.Add(myTask);
+            _context.Add(myTask);
+            _context.SaveChanges();
             return Ok(myTask);
         }
 
         [HttpPut]
         public IActionResult Put(MyTask myTask)
         {
-            var task = _myTasks.FirstOrDefault(x => x.Id == myTask.Id);
+            var task = _context.MyTasks.FirstOrDefault(x => x.Id == myTask.Id);
 
             if (task == null)
             {
@@ -53,20 +57,25 @@ namespace TaskManager.Backend.Controllers
             task.Date = myTask.Date;
             task.IsCompleted = myTask.IsCompleted;
 
+            _context.Update(task);
+            _context.SaveChanges();
+
             return Ok(task);
         }
 
         [HttpDelete("{id:int}")]
         public IActionResult Delete(int id)
         {
-            var task = _myTasks.FirstOrDefault(x => x.Id == id);
+            var task = _context.MyTasks.FirstOrDefault(x => x.Id == id);
 
             if (task == null)
             {
                 return NotFound();
             }
 
-            _myTasks.Remove(task);
+            _context.Remove(task);
+            _context.SaveChanges();
+
             return NoContent();
         }
 
---
+++ TaskManager.Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Share.Entities;
+
+namespace TaskManager.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+		}
+
+		public DbSet<MyTask> MyTasks { get; set; }
+	}
+}
